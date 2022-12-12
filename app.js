c3759a96d0b2bb97c8ca5eb6b97d2b24a3478367
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2022-12-12 09:28:56 +0100

    created project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+//List to carry the carddeck.
+let cardDeck = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+
+//Testing if there is connection
+app.get('/api/test', (req, res) => {
+    res.status(200).send("Der er hul igennem")
+});
+
+
+// Create cardDeck Factory
+
+// Mix cards with (random number. Not the normal random!)
+// Add cards to stack.
