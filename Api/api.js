bd7201d2a2d287836b48f7497876bb1541a53dfd
Webzonedk <webzone@kpdesign.dk>
2023-01-09 14:42:22 +0100

    emergency 2

--- Api/api.js
+++ Api/api.js
@@ -37,17 +37,10 @@ app.get('/api/GetAES', (req, res) => {
 //create new user
 app.post('/api/CreateUser', (req, res) => {
 
-  //test encrypted user
-  const userName = req.body.userName;
-const encrypted = encryption.EncryptAES(userName);
+  const encryptedUser = req.body.userName;
 
-
- 
-const decryptedUser = encryption.DecryptAES(encrypted);
-console.log(decryptedUser);
+  const decryptedUser = encryption.DecryptAES(encryptedUser);
   lobby.CreateUser(decryptedUser);
-
-  console.log("poker tables: ",lobby.pokerTables);
 
   res.status(200).send("user created!");
 
@@ -106,7 +99,6 @@ app.get('/api/testCardDeck', (req, res) => {
 
 //test rulemanager
 app.get('/api/ruleManagerTest', (req, res) => {
-
 
   const ruleManager = RuleManager.getInstance();
   const PokerTable = require('./LogicHandlers/PokerTable');
@@ -171,69 +163,6 @@ app.get('/api/ruleManagerTest', (req, res) => {
   let result = ruleManager.CompareHands(pokerTable);
 
   res.status(200).send(result);
-=======
-  const ruleManager = require('./Managers/RuleManager.js');
-  const PokerTable =  require('./LogicHandlers/PokerTable');
- // const User = require('./Models/User');
-  
-  let hand8= ["2H", "2D", "2C", "kD", "kD", "10D", "3C"];//full house
-  let hand7= ["2D", "3D", "4D", "5D", "8D", "10H", "kC"];//flush
-  let hand6= ["2D", "3D", "4D", "5D", "6D", "10D", "3C"];//straight flush
-  let hand5= ["2H", "3D", "4C", "5C", "6D", "10D", "3C"];//straight
-  let hand4= ["2H", "2D", "2C", "kC", "qD", "10D", "3C"];//three of a kind
-  let hand3= ["2H", "2D", "4C", "4D", "6D", "10D", "3C"];//two pairs
-  let hand2= ["2H", "3D", "4C", "5C", "8D", "10D", "10C"];//one pair
-  let hand1= ["2H", "3D", "aC", "5C", "8D", "10D", "kC"];//highest card
-
-
-
-  let hand_8= ["5D", "3C", "10C", "jC", "qC", "kC", "8H"];//flush
-  let hand_7= ["2D", "9C", "10C", "jC", "qC", "kC", "8H"];//straight flush
-  let hand_6= ["3H", "aD", "10C", "jC", "qC", "kC", "8H"];//straight
-  let hand_5= ["2H", "aH", "10C", "jC", "qC", "kC", "8H"];//straight
-  let hand_4= ["jH", "jD", "10C", "jC", "qC", "kC", "8H"];//three of a kind
-  let hand_3= ["jH", "qD", "10C", "jC", "qC", "kC", "8H"];//two pairs
-  let hand_2= ["2H", "kD", "10C", "jC", "qC", "kC", "8H"];//one pair
-  let hand_1= ["aH", "3D", "10C", "jC", "qC", "kC", "8H"];//highest card
-
-
- //let result = ruleManager.analyzeHand(hand_1);
-
-
-
- //-----------------------------------------------
- //Creating a testPokerTable object
- //-----------------------------------------------
-// testPocketCards=[["aH", "3D"],["2H", "kD"],["jH", "qD"],["jH", "jD"],["2H", "aH"],["3H", "aD"]]
-testPocketCards=[["8D", "9H"],["aH", "9D"],["aD", "8C"],["aS", "9C"],["aC", "4C"]]
-
-//testing compare hands
-let pokerTable = new PokerTable();
-
-//create test users
-for (let i = 0; i < 5; i++) {
-  
-  let user = new User();
-  user.UserID = i;
-  user.UserName = "user_" + i+1;
-  user.Saldo = 1000;
-  user.PocketCards=testPocketCards[i]
-  
-  
-  //add test users to poker table
-  pokerTable.users.push(user);
-  
-}
-// pokerTable.collectiveCards=["10C", "jC", "qC", "kC", "8H"];
-pokerTable.collectiveCards=["10C", "jC", "qC", "kC", "8H"];
-
- //-----------------------------------------------
- //-----------------------------------------------
-
-let result = ruleManager.CompareHands(pokerTable);
-
- res.status(200).send(result);
-
 });
 
 //test cleaning lady
